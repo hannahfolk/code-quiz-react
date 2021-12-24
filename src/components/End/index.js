61f8d96027f3488d40ccdc325bffe02edd273fd9
Hannah Folk <hfolk25@gmail.com>
2021-12-24 15:58:25 -0800

    setting up ending page

---
+++ src/components/End/index.js
@@ -0,0 +1,28 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+
+const End = (props) => {
+  const { secondsLeft, initials, handleInputChange, handleInitialsSubmit } =
+    props;
+
+  return (
+    <Container className="end-container">
+      <h1>All done!</h1>
+      <p>Your final score is {secondsLeft}.</p>
+      <Form onSubmit={handleInitialsSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Enter Initials:</Form.Label>
+          <Form.Control
+            type="text"
+            value={initials}
+            onChange={handleInputChange}
+          />
+          <Button type="submit">Submit</Button>
+        </Form.Group>
+      </Form>
+    </Container>
+  );
+};
+
+export default End;
