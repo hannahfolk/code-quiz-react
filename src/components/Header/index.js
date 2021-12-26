15742e723c7083f22bfab1ce27dcd8b928e6b69e
Hannah Folk <hfolk25@gmail.com>
2021-12-25 20:20:48 -0800

    adjusting header to work for both quiz and highscores pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,13 +2,20 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 const Header = (props) => {
-  const { secondsLeft } = props;
+  const { page, secondsLeft } = props;
+  
   return (
     <Navbar>
-      <Nav.Link href="/highscores">View Highscores</Nav.Link>
-      <p className="time-span">
-        Time Left:<span> {secondsLeft}</span>
-      </p>
+      {page === "quiz" ? (
+        <>
+          <Nav.Link href="/highscores">View Highscores</Nav.Link>
+          <p className="time-span">
+            Time Left:<span> {secondsLeft}</span>
+          </p>
+        </>
+      ) : (
+        <Nav.Link href="/">Go to Quiz</Nav.Link>
+      )}
     </Navbar>
   );
 };
