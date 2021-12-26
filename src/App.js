6e746e1b4f4049de33b8807bdcf147509f3202c0
Hannah Folk <hfolk25@gmail.com>
2021-12-25 20:21:36 -0800

    migrating quiz page out to separate component

--- src/App.js
+++ src/App.js
@@ -1,99 +1,27 @@
 import { useState } from "react";
-import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Header from "./components/Header";
-import Intro from "./components/Intro";
-import Questions from "./components/Questions";
-import End from "./components/End";
+import Quiz from "./pages/Quiz";
+import Highscores from "./pages/Highscores";
 
-import questions from "./questions.json";
 import "./App.css";
 
 const App = () => {
-  let [timer, setTimer] = useState("");
-  let [secondsLeft, setSecondsLeft] = useState(75);
-  let [questionIndex, setQuestionIndex] = useState(0);
-  const [showIntro, setShowIntro] = useState(true);
-  const [showQuestions, setShowQuestions] = useState(false);
-  const [showRightWrong, setShowRightWrong] = useState(false);
-  const [rightWrong, setRightWrong] = useState("");
-  const [showEnd, setShowEnd] = useState(false);
-  const [initials, setInitials] = useState("");
-
-  const startTimer = () => {
-    let timer = setInterval(() => {
-      secondsLeft--;
-
-      setSecondsLeft(secondsLeft);
-
-      if (secondsLeft <= 0) {
-        clearInterval(timer);
-      }
-    }, 1000);
-
-    setTimer(timer);
-  };
-
-  const handleStartClick = () => {
-    startTimer();
-    setShowIntro(false);
-    setShowQuestions(true);
-  };
-
-  const handleAnswerClick = (event) => {
-    event.target.value === questions[questionIndex].correct
-      ? setRightWrong("Correct!")
-      : setRightWrong("Wrong!");
-
-    setShowRightWrong(true);
-
-    setTimeout(() => {
-      setShowRightWrong(false);
-    }, 1000);
-
-    secondsLeft -= 10;
-    setSecondsLeft(secondsLeft);
-
-    questionIndex++;
-    setQuestionIndex(questionIndex);
-
-    if (questionIndex >= 5) {
-      clearInterval(timer);
-      setShowQuestions(false);
-      setShowEnd(true);
-    }
-  };
-
-  const handleInputChange = (event) => {
-    const { value } = event.target.value;
-    setInitials(value);
-  };
-
-  const handleInitialsSubmit = (event) => {
-    event.preventDefault();
-  };
+  const [scores, setScores] = useState(
+    JSON.parse(localStorage.getItem("scores")) || []
+  );
 
   return (
-    <>
-      <Header secondsLeft={secondsLeft} />
-      {showIntro ? <Intro handleStartClick={handleStartClick} /> : null}
-      {showQuestions ? (
-        <Questions
-          questions={questions}
-          questionIndex={questionIndex}
-          handleAnswerClick={handleAnswerClick}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Quiz scores={scores} />} />
+        <Route
+          exact
+          path="/highscores"
+          element={<Highscores scores={scores} />}
         />
-      ) : null}
-      {showRightWrong ? <p className="right-wrong">{rightWrong}</p> : null}
-      {showEnd ? (
-        <End
-          secondsLeft={secondsLeft}
-          initials={initials}
-          handleInputChange={handleInputChange}
-          handleInitialsSubmit={handleInitialsSubmit}
-        />
-      ) : null}
-    </>
+      </Routes>
+    </Router>
   );
 };
 
